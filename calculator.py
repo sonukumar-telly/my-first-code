09b5f4f1dee46851f2c61dbccdbe82b132b6f303
Sonu kumar <kumarsonu777000@gmail.com>
2026-05-19 12:55:38 +0530

    Added my python program- calculator

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a = 25
+b = 5
+
+print("=== Sonu ka Calculator ===")
+print("Pehla number:", a)
+print("Doosra number:", b)
+print("Jod:", a + b)
+print("Ghata:", a - b) 
+print("Guna:", a * b)
+print("Bhaag:", a / b)
+   
