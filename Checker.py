f25f2bb02aa6cfdd0e3c9b33f9badeb6a7846159
Sonu kumar <kumarsonu777000@gmail.com>
2026-05-19 13:15:55 +0530

    My python program- Checker

---
+++ Checker.py
@@ -0,0 +1,11 @@
+age = int(input("Bhai apni age bata: "))
+
+if age >= 18:
+    print("Tu vote de sakta hai. Tu bada ho gaya bhai 🔥")
+    print("GitHub coder + Voter = Double power")
+else:
+    print("Abhi tu chhota hai. Padhai kar le 😂")
+    print("Par coding me to bada hai already 💯")
+
+print("Teri age:", age)
+   
